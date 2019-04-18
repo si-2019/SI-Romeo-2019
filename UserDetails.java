538295220b947ae92a32f1cab96be8133b18358e
djureta1 <djureta1@etf.unsa.ba>
2019-04-18 23:24:41 +0200

    proba

---
+++ UserDetails.java
@@ -0,0 +1,6 @@
+package com.auth0.samples.authapi.springbootauthupdated.user;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
