ff335b2de1ba1ea73a0ce10576fa1d15919e6522
djureta1 <djureta1@etf.unsa.ba>
2019-04-18 23:48:59 +0200

    Postavljanje getera i setera za password

--- UserDetails.java
+++ UserDetails.java
@@ -25,4 +25,12 @@ public class UserDetails{
     public void setUsername(String username) {
         this.username= username;
     } 
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password=password;
+    }
 }
