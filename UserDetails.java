38c80e1b85f5d1c8ac387f8f34e61ad43a178d9a
djureta1 <djureta1@etf.unsa.ba>
2019-04-18 23:41:39 +0200

    Dodavanje osnovnih atributa u klasu

--- UserDetails.java
+++ UserDetails.java
@@ -4,3 +4,13 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+
+@Entity
+public class UserDetails{
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+
+    private long id;
+    private String username;
+    private String password;
+}
