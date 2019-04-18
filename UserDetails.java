9aede631bc5e2809743afe24266781edbe420138
djureta1 <djureta1@etf.unsa.ba>
2019-04-18 23:47:04 +0200

    Postavljanje getera i setera za username

--- UserDetails.java
+++ UserDetails.java
@@ -17,4 +17,12 @@ public class UserDetails{
     public long getID() {
         return id;
     }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username= username;
+    } 
 }
