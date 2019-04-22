d0506d20d18509b61a3fc5615f5d3e4257d368ab
acogic1 <acogic1@etf.unsa.ba>
2019-04-22 18:51:04 +0200

    Metodom addHeader dodan token

--- JWTAuthorizationFilter.java
+++ JWTAuthorizationFilter.java
@@ -58,6 +58,6 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .withSubject(((User) auth.getPrincipal()).getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .sign(HMAC512(SECRET.getBytes()));
-        
+			res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
     }
 }
