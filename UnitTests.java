53ffcef896242f2eb07e0391f815fcd3208f4f66
djureta1 <djureta1@etf.unsa.ba>
2019-04-28 02:04:46 +0200

    Dodani unit testovi

---
+++ UnitTests.java
@@ -0,0 +1,44 @@
+package guru.springframework.unittest.asserts;
+import org.junit.Test;
+import java.util.Map;
+import static org.junit.Assert.*;
+
+
+public class UserDetailsTest {
+    @Test
+    public void testValidUsername() throws Exception {
+        /*Arrange*/
+        UserDetail user=new UserDetails());
+        /*Act*/
+        boolean result = user.isValidUsername("[A-Za-z0-9]*");
+        /*Assert*/
+        assertTrue("Valid username ID failed ", result );
+    }
+    @Test
+    public void testInvalidUsername() throws Exception {
+        /*Arrange*/
+        UserDetails user=new UserDetails();
+        /*Act*/
+        boolean result= user.isValidUsername("[A-Za-z0-9]*");
+        /*Assert*/
+        assertFalse("Invalid username ID passed ", result);
+    }
+    @Test
+    public void testInvalidPassword() throws Exception {
+        /*Arrange*/
+       UserDetails user=new UserDetails();
+        /*Act*/
+        boolean result= user.isValidPassword("[A-Za-z0-9]*{8,}");
+        /*Assert*/
+        assertFalse("Invalid Password passed ", result);
+    }
+     @Test
+    public void testValidPassword() throws Exception {
+        /*Arrange*/
+        UserDetail user=new UserDetails());
+        /*Act*/
+        boolean result = user.isValidPassword("[A-Za-z0-9]*{8,}");
+        /*Assert*/
+        assertTrue("Valid Password failed ", result );
+    }
+}
