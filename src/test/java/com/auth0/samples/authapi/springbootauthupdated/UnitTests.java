4c9774b795ea24c8e60074bbb038bd75a0c62856
djureta1 <djureta1@etf.unsa.ba>
2019-05-15 13:00:51 +0200

    Prepravke unit testa za neispravan Username

--- src/test/java/com/auth0/samples/authapi/springbootauthupdated/UnitTests.java
+++ src/test/java/com/auth0/samples/authapi/springbootauthupdated/UnitTests.java
@@ -17,12 +17,12 @@ public class UnitTests {
     }
     @Test
     public void testInvalidUsername() throws Exception {
-        /*Arrange*/
+
         UserDetails user=new UserDetails();
-        /*Act*/
-        //boolean result= user.isValidUsername("[A-Za-z0-9]*");
-        /*Assert*/
-        //assertFalse("Invalid username ID passed ", result);
+        user.setUsername("Neko");
+        String name="Nekic";
+
+        assertThat(name.equals(user.getUsername())).isFalse();
     }
     @Test
     public void testInvalidPassword() throws Exception {
@@ -36,7 +36,7 @@ public class UnitTests {
     }
      @Test
     public void testValidPassword() throws Exception {
-        /*Arrange*/
+
         UserDetails user=new UserDetails();
         user.setPassword("user123");
         String password= "user123";
@@ -45,25 +45,6 @@ public class UnitTests {
 
         assertThat(password.equals(trenutniPass)).isTrue();
 
-    }
-
-
-
-    @Test
-    public void testInvalidID() throws Exception {
-        /*Arrange*/
-       UserDetails user=new UserDetails();
-
-    }
-
-    @Test
-    public void testValidID() throws Exception {
-        /*Arrange*/
-       UserDetails user=new UserDetails();
-        /*Act*/
-        //boolean result= user.isValidID("[0-9]*");
-        /*Assert*/
-        //assertFalse("Valid ID passed ", result);
     }
     
 }
