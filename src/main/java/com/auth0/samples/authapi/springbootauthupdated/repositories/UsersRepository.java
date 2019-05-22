357ede268d7123d0409c8c71fa040f7bbe69a34c
djureta1 <djureta1@etf.unsa.ba>
2019-05-22 15:05:11 +0200

    Ispravke

--- src/main/java/com/auth0/samples/authapi/springbootauthupdated/repositories/UsersRepository.java
+++ src/main/java/com/auth0/samples/authapi/springbootauthupdated/repositories/UsersRepository.java
@@ -4,7 +4,7 @@ package com.auth0.samples.authapi.springbootauthupdated.repositories;
 import com.auth0.samples.authapi.springbootauthupdated.models.Korisnik;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends CrudRepository<Korisnik, Long> {
+public interface UsersRepository extends CrudRepository<Korisnik, Long> {
 
     Korisnik findByUsername(String username);
 
