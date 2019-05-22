69e59670f3c409ae51627fab55a6d9f936c28e30
acogic1 <acogic1@etf.unsa.ba>
2019-05-22 15:21:53 +0200

    ispravke JWT klase

--- src/main/java/com/auth0/samples/authapi/springbootauthupdated/security/JWTAuthenticationFilter.java
+++ src/main/java/com/auth0/samples/authapi/springbootauthupdated/security/JWTAuthenticationFilter.java
@@ -2,7 +2,10 @@ package com.auth0.samples.authapi.springbootauthupdated.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.samples.authapi.springbootauthupdated.User.UserDetails;
+import com.auth0.samples.authapi.springbootauthupdated.models.Korisnik;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -15,6 +18,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -24,43 +30,122 @@ import static com.auth0.samples.authapi.springbootauthupdated.security.SecurityC
 import static com.auth0.samples.authapi.springbootauthupdated.security.SecurityConstants.SECRET;
 import static com.auth0.samples.authapi.springbootauthupdated.security.SecurityConstants.TOKEN_PREFIX;
 
+
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+
+
     private AuthenticationManager authenticationManager;
 
+
+
     public JWTAuthenticationFilter(AuthenticationManager authenticationManager) {
+
         this.authenticationManager = authenticationManager;
+
     }
 
+
+
     @Override
+
     public Authentication attemptAuthentication(HttpServletRequest req,
+
                                                 HttpServletResponse res) throws AuthenticationException {
+
         try {
-            UserDetails creds = new ObjectMapper()
-                    .readValue(req.getInputStream(), UserDetails.class);   //todo: use user class from database
+
+            Korisnik creds = new ObjectMapper()
+
+                    .readValue(req.getInputStream(), Korisnik.class);
+
+
 
             return authenticationManager.authenticate(
+
                     new UsernamePasswordAuthenticationToken(
+
                             creds.getUsername(),
+
                             creds.getPassword(),
+
                             new ArrayList<>())
+
             );
+
         } catch (IOException e) {
+
             throw new RuntimeException(e);
+
         }
+
     }
+
+
 
     @Override
+
     protected void successfulAuthentication(HttpServletRequest req,
+
                                             HttpServletResponse res,
+
                                             FilterChain chain,
+
                                             Authentication auth) throws IOException, ServletException {
 
+
+
+        String username = ((User) auth.getPrincipal()).getUsername();
+
+
+
         String token = JWT.create()
+
                 .withSubject(((User) auth.getPrincipal()).getUsername())
+
                 .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+
                 .sign(HMAC512(SECRET.getBytes()));
+
+
+
         //res.addHeader(HEADER_STRING, TOKEN_PREFIX + token);
 
-        //todo: create new result (look from my previous github project!)
+
+
+        LocalDateTime date =
+
+                LocalDateTime.ofInstant(Instant.ofEpochMilli(System.currentTimeMillis() + EXPIRATION_TIME), ZoneId.systemDefault());
+
+
+
+        JSONObject result = new JSONObject();
+
+        try {
+            result.put("user", username);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            result.put("tokenExpiresAt", date);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            result.put("token", TOKEN_PREFIX + token);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        res.getWriter().write(result.toString());
+
+        res.getWriter().flush();
+
+        res.getWriter().close();
+
     }
+
 }
