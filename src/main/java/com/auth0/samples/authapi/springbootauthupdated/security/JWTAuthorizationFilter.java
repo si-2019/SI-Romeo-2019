4b4e1f6826da0866931c4b257ca051db16f2e18d
djureta1 <djureta1@etf.unsa.ba>
2019-05-28 13:57:11 +0200

    Implementacija doFilterInternal

--- src/main/java/com/auth0/samples/authapi/springbootauthupdated/security/JWTAuthorizationFilter.java
+++ src/main/java/com/auth0/samples/authapi/springbootauthupdated/security/JWTAuthorizationFilter.java
@@ -23,7 +23,22 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
         super(authManager);
     }
 
-   
+    @Override
+    protected void doFilterInternal (HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException,
+            ServletException{
+
+        String header=req.getHeader(HEADER_STRING);
+
+        if(header==null || !header.startsWith(TOKEN_PREFIX)){
+            chain.doFilter(req,res);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authentication=getAuthentication(req);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(req, res);
+    }
 
     private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
         String token=request.getHeader(HEADER_STRING);
