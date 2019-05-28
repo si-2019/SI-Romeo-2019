42ad2e2b8cdd2de0acabcf244e9d6cbbde34d155
djureta1 <djureta1@etf.unsa.ba>
2019-05-28 13:39:27 +0200

    Kreiranje klase JWTAuthorizationFilter zajedno sa konstruktorom

---
+++ src/main/java/com/auth0/samples/authapi/springbootauthupdated/security/JWTAuthorizationFilter.java
@@ -0,0 +1,13 @@
+package com.auth0.samples.authapi.springbootauthupdated.security;
+
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+    public JWTAuthorizationFilter(AuthenticationManager authManager){
+        super(authManager);
+    }
+
+    
+}
