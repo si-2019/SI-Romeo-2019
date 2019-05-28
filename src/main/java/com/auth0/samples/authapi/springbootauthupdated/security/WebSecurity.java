71ffb0e7bae779baa9553774279f52894298fbef
tariqzayn <abdurrazaky@gmail.com>
2019-05-28 13:38:11 +0200

    created configure method

--- src/main/java/com/auth0/samples/authapi/springbootauthupdated/security/WebSecurity.java
+++ src/main/java/com/auth0/samples/authapi/springbootauthupdated/security/WebSecurity.java
@@ -1,8 +1,10 @@
 package com.auth0.samples.authapi.springbootauthupdated.security;
 
 import com.auth0.samples.authapi.springbootauthupdated.User.UserDetailsServiceImpl;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -10,5 +12,17 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
 
     public WebSecurity(UserDetailsServiceImpl userDetailsService) {
         this.userDetailsService = userDetailsService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers().permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                // this disables session creation on Spring Security
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
